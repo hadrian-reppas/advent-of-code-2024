b3b06d9cf4398b2806e6fa6961d0053a46be38b2
Hadrian <hadrian@uchicago.edu>
2024-12-18 00:14:29 -0500

    add z3 code

--- day17/day17.cpp
+++ day17/day17.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <cmath>
 
+#include "z3++.h"
+
 #include "../util/util.h"
 
 struct Computer {
@@ -105,6 +107,116 @@ struct Computer {
     }
 };
 
+struct Translator {
+    z3::context ctx;
+    z3::optimize opt;
+    z3::sort bv_sort;
+    z3::expr a, a0, b, c;
+    std::vector<long> ops;
+    long an, bn, cn;
+
+    explicit Translator(const std::string &input)
+        : opt{ctx},
+          bv_sort{ctx.bv_sort(64)},
+          a{ctx.constant("a0", bv_sort)},
+          a0{a},
+          b{ctx.constant("b0", bv_sort)},
+          c{ctx.constant("c0", bv_sort)},
+          an{0},
+          bn{0},
+          cn{0} {
+        ops = Computer{input}.ops;
+    }
+
+    z3::expr constant(const long arg) {
+        return ctx.bv_val(static_cast<int>(arg), 64);
+    }
+
+    z3::expr combo(const long arg) {
+        if (arg <= 3)
+            return constant(arg);
+        if (arg == 4)
+            return a;
+        if (arg == 5)
+            return b;
+        if (arg == 6)
+            return c;
+        throw std::invalid_argument("invalid argument");
+    }
+
+    z3::expr freshA() {
+        return a = ctx.constant(std::format("a{}", ++an).c_str(), bv_sort);
+    }
+
+    z3::expr freshB() {
+        return b = ctx.constant(std::format("b{}", ++bn).c_str(), bv_sort);
+    }
+
+    z3::expr freshC() {
+        return c = ctx.constant(std::format("c{}", ++cn).c_str(), bv_sort);
+    }
+
+    void compilePass(const long out, const bool last) {
+        bool seenOut = false;
+        for (long i = 0; i < ops.size() - 2; i += 2) {
+            const auto op = ops[i];
+            const auto arg = ops[i + 1];
+
+            switch (op) {
+                case 1: {
+                    const auto bb = b ^ constant(arg);
+                    opt.add(freshB() == bb);
+                    break;
+                }
+                case 2: {
+                    const auto bb = combo(arg) & constant(7);
+                    opt.add(freshB() == bb);
+                    break;
+                }
+                case 4: {
+                    const auto bb = b ^ c;
+                    opt.add(freshB() == bb);
+                    break;
+                }
+                case 5: {
+                    assert(!seenOut); // Only see one out instruction
+                    seenOut = true;
+                    const auto o = combo(arg) & constant(7);
+                    opt.add(o == constant(out));
+                    break;
+                }
+                case 0:
+                case 6:
+                case 7: {
+                    const auto res = lshr(a, combo(arg));
+                    if (op == 0)
+                        opt.add(freshA() == res);
+                    else if (op == 6)
+                        opt.add(freshB() == res);
+                    else
+                        opt.add(freshC() == res);
+                    break;
+                }
+                default:
+                    assert(false);
+            }
+        }
+
+        assert(ops[ops.size() - 2] == 3); // Last instruction is jnz
+        assert(ops[ops.size() - 1] == 0); // Jump goes to 0
+        if (last)
+            opt.add(a == constant(0));
+        else
+            opt.add(a != constant(0));
+    }
+
+
+    void compile() {
+        for (long i = 0; i < ops.size(); i++)
+            compilePass(ops[i], i == ops.size() - 1);
+    }
+};
+
 namespace day17 {
     std::string part1(const std::string &input) {
         Computer computer{input};
@@ -112,7 +224,14 @@ namespace day17 {
         return computer.output();
     }
 
-    long part2(const std::string &input [[maybe_unused]]) {
-        return 202972175280687;
+    long part2(const std::string &input) {
+        Translator translator{input};
+        translator.compile();
+
+        translator.opt.minimize(translator.a0);
+        const auto res = translator.opt.check();
+        assert(res == z3::sat);
+        const z3::model model = translator.opt.get_model();
+        return model.eval(translator.a0).as_int64();
     }
 }
