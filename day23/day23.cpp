bb3480d21ad2c842bdaa5a4e4a2550fc4ec03706
Hadrian <hadrian@uchicago.edu>
2025-01-01 11:03:40 -0500

    implement Bron–Kerbosch

--- day23/day23.cpp
+++ day23/day23.cpp
@@ -5,7 +5,9 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <print>
+#include <random>
+#include <set>
+#include <array>
 
 #include "../util/util.h"
 
@@ -36,6 +38,90 @@ static std::vector<std::array<std::string, 3> > getTriangles(
     return triangles;
 }
 
+static std::unordered_set<std::string_view> intersection(const std::unordered_set<std::string_view> &a,
+                                                         const std::unordered_set<std::string_view> &b) {
+    std::unordered_set<std::string_view> result;
+    for (const auto &s: a) {
+        if (b.contains(s))
+            result.insert(s);
+    }
+    return result;
+}
+
+static std::unordered_set<std::string_view> difference(const std::unordered_set<std::string_view> &a,
+                                                       const std::unordered_set<std::string_view> &b) {
+    std::unordered_set<std::string_view> result;
+    for (const auto &s: a) {
+        if (!b.contains(s))
+            result.insert(s);
+    }
+    return result;
+}
+
+static std::unordered_set<std::string_view> with(const std::unordered_set<std::string_view> &x,
+                                                 const std::string_view s) {
+    std::unordered_set<std::string_view> result = x;
+    result.insert(s);
+    return result;
+}
+
+static std::string_view sample(const std::unordered_set<std::string_view> &p,
+                               const std::unordered_set<std::string_view> &x) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<> prob_dist(0, 1);
+    const auto prob = static_cast<double>(p.size()) / static_cast<double>(p.size() + x.size());
+    const auto fromP = prob_dist(gen) < prob;
+
+    const auto &set = fromP ? p : x;
+
+    const std::vector<std::string_view> elements(set.begin(), set.end());
+    std::uniform_int_distribution<> index_dist(0, static_cast<int>(elements.size() - 1));
+    return elements[index_dist(gen)];
+}
+
+static void search(
+    const std::unordered_map<std::string_view, std::unordered_set<std::string_view> > &graph,
+    const std::unordered_set<std::string_view> &r,
+    const std::unordered_set<std::string_view> &p,
+    const std::unordered_set<std::string_view> &x,
+    std::vector<std::unordered_set<std::string_view> > &out) {
+    if (p.empty() && x.empty()) {
+        out.push_back(r);
+        return;
+    }
+
+    auto pMut = p;
+    auto xMut = x;
+    const auto u = sample(p, x);
+    for (const auto v: difference(p, graph.at(u))) {
+        search(graph, with(r, v), intersection(p, graph.at(v)), intersection(x, graph.at(v)), out);
+        pMut.erase(v);
+        xMut.insert(v);
+    }
+}
+
+static std::unordered_set<std::string_view> maxClique(
+    const std::unordered_map<std::string, std::unordered_set<std::string> > &rawGraph) {
+    std::unordered_map<std::string_view, std::unordered_set<std::string_view> > graph;
+    std::unordered_set<std::string_view> r, p, x;
+    for (const auto &[node, neighbors]: rawGraph) {
+        p.insert(node);
+        for (const auto &neighbor: neighbors)
+            graph[node].insert(neighbor);
+    }
+
+    std::vector<std::unordered_set<std::string_view> > out;
+    search(graph, r, p, x, out);
+    long maxClique = 0;
+    for (long i = 1; i < out.size(); i++) {
+        if (out[i].size() > out[maxClique].size())
+            maxClique = i;
+    }
+    return out[maxClique];
+}
+
 namespace day23 {
     long part1(const std::string &input) {
         const auto graph = parseInput(input);
@@ -49,6 +135,15 @@ namespace day23 {
     }
 
     std::string part2(const std::string &input) {
-        return "av,fr,gj,hk,ii,je,jo,lq,ny,qd,uq,wq,xc"; // TODO: Implement Bron–Kerbosch
+        const auto graph = parseInput(input);
+        const auto clique = maxClique(graph);
+        const std::set<std::string_view> sorted(clique.begin(), clique.end());
+        std::string result;
+        for (const auto &node: sorted) {
+            if (!result.empty())
+                result += ",";
+            result += node;
+        }
+        return result;
     }
 }
